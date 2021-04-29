2b90ee9a2059c58ad22884208ec973add800a46c
kubanekzsolt <63916757+kubanekzsolt@users.noreply.github.com>
2021-04-29 12:50:45 +0200

    JurassicPark completed
    With PreparedStatement interface

--- src/main/java/hu/nive/ujratervezes/jurassic/JurassicPark.java
+++ src/main/java/hu/nive/ujratervezes/jurassic/JurassicPark.java
@@ -1,5 +1,15 @@
 package hu.nive.ujratervezes.jurassic;
 
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class JurassicPark {
 
     private String dbUrl;
@@ -12,4 +22,20 @@ public class JurassicPark {
         this.dbPassword = dbPassword;
     }
 
+	public List<String> checkOverpopulation() {
+		List<String> ret = new ArrayList();
+        try (Connection connection = DriverManager.getConnection(dbUrl, dbUser, dbPassword)) {
+            String query = "select breed from dinosaur where actual > expected";
+            PreparedStatement statement = connection.prepareStatement(query);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+            	ret.add(resultSet.getString("breed"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        Collections.sort(ret);
+		return ret;
+	}
 }
